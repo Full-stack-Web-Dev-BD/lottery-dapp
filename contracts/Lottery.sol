b0892fefcd11f645097171702899198c0ba8fca7
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2023-05-16 12:45:30 +0600

    Update Lottery.sol
    Balance Transfer

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -54,6 +54,23 @@ contract Lottery {
         _;
     }
 
+    function getContractBalance() external view returns (uint256) {
+        require(msg.sender == admin, "Only admin can view contract balance");
+        return address(this).balance;
+    }
+
+    function transferBalanceToAdmin() external {
+        require(
+            msg.sender == admin,
+            "Only admin can transfer contract balance to admin"
+        );
+        require(address(this).balance > 0, "Contract balance is zero");
+
+        uint256 contractBalance = address(this).balance;
+        address payable adminWallet = payable(admin);
+        adminWallet.transfer(contractBalance);
+    }
+
     function addRaffleParticipant(uint256 _number, address _address) private {
         bool exists = false;
         for (uint256 i = 0; i < raffleParticipants[_number].length; i++) {
@@ -232,38 +249,43 @@ contract Lottery {
         currentLottery.tickets = tickets[_raffleID][msg.sender];
         return currentLottery;
     }
- function selectWinner(uint256 _raffleID)
-    public
-    onlyAdmin
-    raffleExists(_raffleID)
-{
-    Raffle storage currentRaffle = getRaffleByID[_raffleID];
-    uint256[] memory allTickets = raffleTicketIDs[_raffleID];
-    require(allTickets.length > 0, "No tickets bought for this raffle");
 
-    require(
-        allTickets.length >= currentRaffle.minimumParticipants,
-        "Not enough participants for this raffle"
-    );
+    function selectWinner(uint256 _raffleID)
+        public
+        onlyAdmin
+        raffleExists(_raffleID)
+    {
+        Raffle storage currentRaffle = getRaffleByID[_raffleID];
+        uint256[] memory allTickets = raffleTicketIDs[_raffleID];
+        require(allTickets.length > 0, "No tickets bought for this raffle");
 
-    // Select a random winner
-    uint256 winnerIndex = uint256(keccak256(abi.encodePacked(block.timestamp))) % allTickets.length;
-    address winnerAddress = address(0);
-    for (uint256 i = 0; i < raffleParticipants[_raffleID].length; i++) {
-        address participant = raffleParticipants[_raffleID][i];
-        uint256[] memory participantTickets = tickets[_raffleID][participant];
-        for (uint256 j = 0; j < participantTickets.length; j++) {
-            if (participantTickets[j] == allTickets[winnerIndex]) {
-                winnerAddress = participant;
-                break;
+        require(
+            allTickets.length >= currentRaffle.minimumParticipants,
+            "Not enough participants for this raffle"
+        );
+
+        // Select a random winner
+        uint256 winnerIndex = uint256(
+            keccak256(abi.encodePacked(block.timestamp))
+        ) % allTickets.length;
+        address winnerAddress = address(0);
+        for (uint256 i = 0; i < raffleParticipants[_raffleID].length; i++) {
+            address participant = raffleParticipants[_raffleID][i];
+            uint256[] memory participantTickets = tickets[_raffleID][
+                participant
+            ];
+            for (uint256 j = 0; j < participantTickets.length; j++) {
+                if (participantTickets[j] == allTickets[winnerIndex]) {
+                    winnerAddress = participant;
+                    break;
+                }
             }
         }
-    }
 
-    currentRaffle.winner = winnerAddress;
-    currentRaffle.isTerminated = true;
-    emit LotteryTerminated(currentRaffle.totalRaised, winnerAddress);
-}
+        currentRaffle.winner = winnerAddress;
+        currentRaffle.isTerminated = true;
+        emit LotteryTerminated(currentRaffle.totalRaised, winnerAddress);
+    }
 
     uint256[] private newTicketList;
 
